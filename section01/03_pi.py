13f423f37bd1d00307013c95d06f850af02e14b2
Mikiya Abe <s15ti032@gmail.com>
2018-01-20 00:49:18 +0900

    Create section01/

---
+++ section01/03_pi.py
@@ -0,0 +1,11 @@
+def pi():
+    word = []
+    char_len = []
+    word = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".split()
+    
+    for w in word:
+        char_len.append(len(w))
+
+    print(char_len)
+
+pi()
