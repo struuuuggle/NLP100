b66f064d721ee1a1c05a31dd34e97068edf45c57
Mikiya Abe <s15ti032@gmail.com>
2018-01-25 03:37:04 +0900

    Update sec11_replace.py

--- ch02/sec11_replace.py
+++ ch02/sec11_replace.py
@@ -7,6 +7,8 @@ def main():
     inFile = open('../src/hightemp.txt', 'r')
     outFile = open('../src/sec11.txt', 'w')
     replace(inFile, outFile)
+    # 以下のコマンドで確認
+    # cat ../src/hightemp.txt | tr "\t" " "
 
 if __name__ == '__main__':
     main()
