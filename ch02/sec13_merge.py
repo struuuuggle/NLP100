45be59fe29afc3069cbfc5c11f9b1f6cd591dbc8
Mikiya Abe <s15ti032@gmail.com>
2018-02-09 20:21:04 +0900

    Add sec13_merge.py

---
+++ ch02/sec13_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def merge(col1_filename,col2_filename):
+    f1, f2 = open(col1_filename, 'r'), open(col2_filename, 'r')
+    f3 = open('../src/sec13.txt', 'a')
+    for w1, w2 in zip(f1.read().split('\n'), f2.read().split('\n')):
+        f3.write(w1  + '\t' + w2 + '\n')
+    f1.close(), f2.close(), f3.close()
+
+def main():
+    col1_filename, col2_filename = '../src/col1.txt', '../src/col2.txt'
+    merge(col1_filename, col2_filename)
+
+if __name__ == '__main__':
+    main()
