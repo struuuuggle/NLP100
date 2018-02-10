d5c910ccba3ff23e9705162b47f55be69612cbe0
Mikiya Abe <s15ti032@gmail.com>
2018-02-11 00:28:25 +0900

    Add sec16_split.py

--- ch02/sec10_countRow.py
+++ ch02/sec10_countRow.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-def countRow(file):
+def count_line(filename):
+    f = open(filename, 'r')
     sum = 0
-    for x in file:
+    for x in f:
         sum += 1
     return sum
 
 def main():
-    file = open('../src/hightemp.txt')
-    print(countRow(file))
+    filename = '../src/hightemp.txt'
+    print(count_line(filename))
 
 if __name__ == '__main__':
     main()
---
+++ ch02/sec16_split.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import sys
+from sec10_count_line import count_line
+
+def split(filename, n):
+    """
+    fileをn分割して、div行ずつn個のファイルにそれぞれ書き込む
+    """
+    src_f = open(filename, 'r')
+    src_list = src_f.read().split('\n')
+    div = int(countRow(filename) / n)
+    for i in range(n):
+        out_f = open(('./test/sec16_xa' + chr(ord('a') + i) + '.txt'), 'w')
+        out_f.write('\n'.join(src_list[div*i:div*(i+1)]))
+        out_f.close()
+    src_f.close()
+
+def main():
+    filename = '../src/hightemp.txt'
+    n = int(sys.argv[1])
+    split(filename, n)
+
+if __name__ == '__main__':
+    main()
