adff1d3555e1c4bd3e1f2a9ce8efc1685cbf6293
Mikiya Abe <s15ti032@gmail.com>
2018-02-20 16:11:01 +0900

    Update sec27_innerlink.py

--- src/ch03/sec27_innerlink.py
+++ src/ch03/sec27_innerlink.py
@@ -3,9 +3,7 @@ import re
 from sec26_markup import remove_markup
 
 def innerlink():
-    # [[記事名|表示文字]]の形式の時には記事名は取り出される
-    # できれば表示文字を取り出したい
-    innerlink_regex = re.compile(r'\[\[(.+?)(\|.+)?\]\]')
+    innerlink_regex = re.compile(r'\[\[(?:.*?\|)*(.+?)\]\]')
     dict = remove_markup()
     for k, v in zip(dict.keys(), dict.values()):
         dict[k] = innerlink_regex.sub(r'\1', v)
