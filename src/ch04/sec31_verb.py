b1aa9d852f431b6d215431fcf332f2edf6e208bc
Mikiya Abe <s15ti032@gmail.com>
2018-03-01 04:25:06 +0900

    Refactoring

--- src/ch04/sec31_verb.py
+++ src/ch04/sec31_verb.py
@@ -3,11 +3,15 @@ from sec30_load_mecab import load_mecab
 
 def extract_verb():
     sentences = load_mecab('./test/neko.txt.mecab')
+    verbs = [m['surface'] for s in sentences for m in s if m['pos'] == '動詞']
+    """
+    # 以下の処理と等価
     verbs = []
     for sentence in sentences:
         for morpheme in sentence:
             if morpheme['pos'] == '動詞':
                 verbs.append(morpheme['surface'])
+    """
     return verbs
 
 def main():
