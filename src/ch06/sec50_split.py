e4c33441c1a7d71f5ed80da7605f06c6dabe997b
Mikiya Abe <s15ti032@gmail.com>
2018-03-04 16:01:59 +0900

    Add sec50_split.py

---
+++ src/ch06/sec50_split.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import re
+
+def split(filename):
+    pattern = re.compile(r'(?<=[\.\;\:\?\!])\s+(?=[A-Z])', re.DOTALL | re.MULTILINE)
+    with open(filename, 'r') as f:
+        return [s.strip() for s in re.split(pattern, f.read())]
+
+def main():
+    for sentence in split('../../lib/nlp.txt'):
+        print(sentence)
+
+if __name__ == '__main__':
+    main()
