43bad64022f937f4bca2b7643e00a7ccff37ec34
Mikiya Abe <s15ti032@gmail.com>
2018-03-04 22:18:18 +0900

    Add sec52_stemming.py

---
+++ src/ch06/sec52_stemming.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from sec51_words import words
+from porter2stemmer import Porter2Stemmer
+
+def stemming():
+    stemmer = Porter2Stemmer()
+    for w in words():
+        print(w, stemmer.stem(w), sep='\t')
+
+def main():
+    stemming()
+
+if __name__ == '__main__':
+    main()
