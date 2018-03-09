cca166e79e9a1a37f8435cfb9e28d108d9289ffa
Mikiya Abe <s15ti032@gmail.com>
2018-03-09 20:39:23 +0900

    Add sec43_print_ntov.py

---
+++ src/ch05/sec43_print_ntov.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from sec41_chunk import load_chunk
+
+def find_pos(posname, chunk):
+    for m in chunk.morphs:
+        if m.pos == posname:
+            return True
+    return False
+
+def print_ntov():
+    sentences = load_chunk()
+    for i, chunks in enumerate(sentences):
+        for chunk in chunks:
+            if find_pos('名詞', chunk) and find_pos('動詞', chunks[chunk.dst]):
+                if chunk.dst != -1:
+                    print('{}\t{}'.format(chunk.surface, chunks[chunk.dst].surface))
+
+def main():
+    print_ntov()
+
+if __name__ == '__main__':
+    main()
