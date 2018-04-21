5a6be37b79cd01a611d624b414faca34f951a943
Mikiya Abe <s15ti032@gmail.com>
2018-04-22 02:17:53 +0900

    Add script to download corpus

---
+++ lib/get_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/nlp.txt
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/artist.json.gz
+wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/enwiki-20150112-400-r10-105752.txt.bz2
